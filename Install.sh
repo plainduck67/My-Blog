2151cd903955d606fd6f72f830ad506519db2b90
Gardner Sullivan <gardnersullivan9@gmail.com>
2025-09-10 19:58:10 -0600

    finished installer

--- Install.sh
+++ Install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
+set -e
+
 if command -v dotnet >/dev/null 2>&1; then
     echo ".NET is already installed. Version: $(dotnet --version)"
 else
-    echo ".NET is not installed. Installing..."
-
     if [ -f /etc/debian_version ]; then
         wget https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
         sudo dpkg -i packages-microsoft-prod.deb
@@ -12,9 +12,31 @@ else
         sudo apt-get install -y apt-transport-https dotnet-sdk-8.0
         rm packages-microsoft-prod.deb
     elif [ -f /etc/redhat-release ]; then
-        
         sudo dnf install -y dotnet-sdk-8.0
     else
-        echo "Unsupported distro"
+        echo "Unsupported distro. Install .NET manually."
+        exit 1
     fi
 fi
+
+if command -v git >/dev/null 2>&1; then
+    echo "Git is already installed."
+else
+    if [ -f /etc/debian_version ]; then
+        sudo apt-get update
+        sudo apt-get install -y git
+    elif [ -f /etc/redhat-release ]; then
+        sudo dnf install -y git
+    else
+        echo "Unsupported distro. Install Git manually."
+        exit 1
+    fi
+fi
+
+cd ~
+mkdir sorter
+cd sorter
+git clone https://github.com/plainduck67/fileSorter
+cd fileSorter/sorter
+dotnet run
+
